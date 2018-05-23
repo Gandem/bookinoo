3dfd914a2a947c673d0d612af60c96f02d038631
Arnaud Clauss <arnaud.clauss@student.ecp.fr>
2018-05-23 14:38:26 +0200

    Initial commit Android Studio Project

---
+++ app/Bookinoo/app/src/main/java/com/claussa/bookinoo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.claussa.bookinoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException ignored) {}
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            Log.d("AUTH", currentUser.getDisplayName());
+        } else {
+            Log.d("AUTH", "No user");
+        }
+
+    }
+}
